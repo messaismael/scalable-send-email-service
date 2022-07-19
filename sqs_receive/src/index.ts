b354ef0a488ff557d12b778a8db9d666624ec896
messaismael <messaismael01@gmail.com>
2022-07-19 22:10:38 +0100

    queue + email service

--- sqs_receive/src/index.ts
+++ sqs_receive/src/index.ts
@@ -1,6 +1,26 @@
-const handler = function (event, context) {
+import { Context, APIGatewayEvent } from 'aws-lambda';
+
+import sesSend from "./ses";
+
+const handler = async function (event: APIGatewayEvent, context: Context) {
   console.log("event", event);
-  context.succeed('hello world');
+  const { Records } = event;
+
+  for (let i = 0; i < Records.length; i++) {
+    var formData = JSON.parse(Records[i].body || '{}');
+
+    console.log("form data", formData);
+
+    try {
+      let sesRes = await sesSend(formData);
+      console.log("response", {success:true, messaage:'email sended', data:sesRes});
+    } catch (err) {
+      console.error(err);
+      // do something
+    }
+  }
+
+  return { Status: "finished", success: true };
 };
 
 export { handler };
