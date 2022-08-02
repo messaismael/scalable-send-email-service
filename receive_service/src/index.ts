ea0176453d13ba658c165d6dec1ed0b8409897eb
messaismael <messaismael01@gmail.com>
2022-08-02 01:26:41 +0100

    update message status on DynamoDB after sending

--- receive_service/src/index.ts
+++ receive_service/src/index.ts
@@ -1,33 +1,56 @@
+import AWS from 'aws-sdk';
 import { Context, APIGatewayEvent } from 'aws-lambda';
 
 import sesSend from "./ses";
 import delete_msg from './delete_msg';
 
+
+let dynamoDb = new AWS.DynamoDB.DocumentClient({region:"us-east-1"});
+
 const handler = async function (event: APIGatewayEvent, context: Context) {
-  console.log("event", event);
-  const { Records } = event;
+	console.log("event", event);
+	const { Records } = event;
 
-  for (let i = 0; i < Records.length; i++) {
-    var record = Records[i];
-    var formData = JSON.parse(record.body || '{}');
+	for (let i = 0; i < Records.length; i++) {
+		var record = Records[i];
 
-    try {
-      let sesRes = await sesSend(formData);
-      console.log("Success", { message: 'Email sended' });
+		var params: AWS.DynamoDB.DocumentClient.GetItemInput = {
+			TableName: 'sqsmessagedb',
+			Key: {
+				messageId: record.messageId
+			}
+		};
 
-      try {
-        let deleteRes = await delete_msg(record.receiptHandle);
+		let msgStatus = await dynamoDb.get(params).promise();
+		console.log("message Status", msgStatus)
 
-        console.log("Success", { message: "Message removed from queue" });
-      } catch (err) {
-        console.error("error deleting message", err);
-      }
-    } catch (err) {
-      console.error("error sending via ses", err);
-    }
-  }
+		if (msgStatus.Item.emailSended == "false") {
+			var formData = JSON.parse(record.body || '{}');
 
-  return { Status: "finished", success: true };
+			try {
+				await sesSend(formData);
+				console.log("Success", { message: 'Email sended' });
+
+				msgStatus.Item.emailSended = "true";
+				let putParams: AWS.DynamoDB.DocumentClient.PutItemInput = {
+					TableName: 'sqsmessagedb',
+					Item: { ...msgStatus.Item }
+				}
+				await dynamoDb.put(putParams).promise();
+
+				try {
+					await delete_msg(record.receiptHandle);
+					console.log("Success", { message: "Message removed from queue" });
+				} catch (err) {
+					console.error("error deleting message", err);
+				}
+			} catch (err) {
+				console.error("error sending via ses", err);
+			}
+		}
+	}
+
+	return { Status: "finished", success: true };
 };
 
 export { handler };
